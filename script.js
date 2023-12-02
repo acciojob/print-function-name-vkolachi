f181fd10f48ff3291525a0ba40261f45ca185618
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-12-02 12:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let z=prompt("enter function name ");
-alert(z);
+// let z=prompt("enter function name ");
+// alert(z);
+// function functionName() {
+//     alert(arguments.callee.name);
+// }
+
+// // Calling the function to display its own name
+// functionName();
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+// Prompt the user for the function name
+var customFunctionName = prompt("Enter the function name:");
+
+// Define a new function with the prompted name
+window[customFunctionName] = functionName;
+
+// Call the dynamically named function to display its own name
+window[customFunctionName]();
 
